e9df4826885cc4a5c1ee6ac0250e09a9e5782ddc
MHIGists <milko.hristov.ivanov@gmail.com>
2024-08-30 13:39:59 +0000

    Alter users table ot add color and link fields

---
+++ database/migrations/2024_08_30_131828_add_link_and_color_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('hyperlink')->nullable()->after('remember_token');
+            $table->string('favorite_color', 7)->nullable()->after('hyperlink'); // 7 characters to store hex color
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['hyperlink', 'favorite_color']);
+        });
+    }
+};
