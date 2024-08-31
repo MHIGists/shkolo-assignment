7d974877dc34371325f56c3ad48403d9cbd28383
MHIGists <milko.hristov.ivanov@gmail.com>
2024-08-31 19:11:11 +0000

    Oops can't have nested forms. TODO better style for the settings-form.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,20 +34,19 @@
     @endif
     <div class="p-4 rounded-md shadow-lg max-w-xl mx-auto text-gray-600 ">
         <p class="text-xl font-semibold mb-2">
-            This is my own interpretation of the given task.
+            This is my own interpretation of the given task. I hope I haven't deviated too much.
         </p>
         <p class="mb-2">
             I kept the default login system & profile settings as well as most of the style.
         </p>
         <p class="mb-2">
-            Button settings can be found in the
-            <span class="font-medium italic">User Dropdown</span>
-            ->
-            <span class="font-medium italic">Button settings</span>.
+            Button settings are per-user per-button. There is a "Button Settings" tab that is used to display the current configuration
+            of buttons as well as Update, Reset(update), Delete, Create them. All CRUD operations are implemented.
         </p>
         <p>
             The backend is using Laravel Breeze on a Mariadb server using TailwindCSS. Everything is hosted on my home
-            server(Ubuntu with Apache2).
+            server(Ubuntu with Apache2). There is little custom JS and CSS, however if needed I'll re-do everything to use
+            as little frameworks as possible.
         </p>
     </div>
 </div>
